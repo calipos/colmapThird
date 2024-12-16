980f9183c6007542132fe414e05812811f898746
calipos <calipos@126.com>
2024-12-16 14:01:14 +0800

    0

---
+++ test/test.cpp
@@ -0,0 +1,63 @@
+﻿// test.cpp : Defines the entry point for the application.
+//
+#include <map>
+#include <vector>
+#include <random>
+#include <string> 
+#include <iostream>
+#define EACH_ROW 11
+void show(int a, int b)
+{
+	for (int i = 0; i < EACH_ROW; i++)
+	{
+		if (i == a || i == b)
+		{
+			std::cout << 1;
+		}
+		else
+		{
+			std::cout << ".";
+		}
+	}
+	std::cout << std::endl;
+}
+int main()
+{
+	for (int i = 1; i < 20; i++)
+	{
+		int row = std::ceil(0.5*(sqrt(8*i+1)-1))  ;
+		 
+		int col = row * (row + 1) / 2-i;
+		show(row, col);
+	}
+	return 0;
+	{
+		std::vector<float>data;
+		std::vector<int>dataInt;
+		data.resize(128);
+		dataInt.resize(128);
+		for (size_t i = 0; i < 128; i++)
+		{
+			dataInt[i] = 4 * i;
+			data[i] = dataInt[i]/512.;
+		}
+	
+	}
+	std::vector<float>data;
+	std::vector<int>dataInt;
+	data.resize(128);
+	dataInt.resize(128);
+	float sum = 0;
+	for (int i = 0; i < 128; i++)
+	{
+		data[i] = 1+0.5*i;
+		sum += (data[i] * data[i]);
+	}
+	sum = 1. / sqrt(sum);
+	for (int i = 0; i < 128; i++)
+	{
+		data[i] *= sum; 
+		dataInt[i] = 512*data[i];
+	}
+	return 0;
+}
---
+++ tools/mp42jpg.py
@@ -0,0 +1,56 @@
+
+import cv2
+from cv2 import VideoCapture
+from cv2 import imwrite
+import json
+import os
+import sys
+import math
+import numpy as np
+
+
+def ndArrayToList(data):
+    r, c = data.shape
+    outdata = []
+    if (c == 2):
+        for i in range(r):
+            outdata.append((data[i, 0], data[i, 1]))
+    if (c == 3):
+        for i in range(r):
+            outdata.append((data[i, 0], data[i, 1], data[i, 2]))
+    return outdata
+
+
+def save_image(image, addr, num):
+    address = addr + str(num).zfill(5) + '.jpg'
+    #img_90 = cv2.flip(cv2.transpose(image), 1)
+    #imwrite(address, img_90)
+    imwrite(address, image)
+
+
+if __name__ == '__main__':
+
+    video_path = 'data/4.mp4'
+    time_interval = 2
+    jsonRoot = 'data/'
+
+    ######
+    #time_interval = 5 #时间间隔
+
+    # 读取视频文件
+    videoCapture = VideoCapture(video_path)
+
+    # 读帧
+    success, frame = videoCapture.read()
+    print(success)
+
+    i = 0
+    j = 0
+
+    while success:
+        i = i + 1
+        if (i % time_interval == 0):
+            print('save frame:', i)
+            save_image(frame, jsonRoot, j+0)
+            j = j + 1
+        success, frame = videoCapture.read()
